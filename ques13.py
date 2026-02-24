771058eaaa45f4d3544cf0ce568af6f57aa2b6a7
sameerchoudhary3415 <choudharysameer3415@gmail.com>
2026-02-24 23:21:47 +0530

    Create ques13.py

---
+++ ques13.py
@@ -0,0 +1,12 @@
+class Solution:
+    def merge(self, intervals):
+        intervals.sort()
+        res = []
+
+        for s, e in intervals:
+            if not res or s > res[-1][1]:
+                res.append([s, e])
+            else:
+                res[-1][1] = max(res[-1][1], e)
+
+        return res

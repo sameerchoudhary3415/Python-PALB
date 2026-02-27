12ecc52d703a81c7e2fd390f2749c99ef3c78e6b
sameerchoudhary3415 <choudharysameer3415@gmail.com>
2026-02-27 11:28:09 +0530

    group anagrams

---
+++ ques29.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+       
+        from collections import defaultdict
+        
+        anagram_map = defaultdict(list)
+        
+        for word in strs:
+            # Sort the word to get the key
+            key = ''.join(sorted(word))
+            anagram_map[key].append(word)
+        
+        return list(anagram_map.values())
